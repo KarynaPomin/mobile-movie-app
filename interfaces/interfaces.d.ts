220f4909bbf606b65514f0d5a389a0c1aa8cd25b
Karyna Pomin <115844791+KarynaPomin@users.noreply.github.com>
2026-08-06 19:41:18 +0200

    Implement tab navigation and routing.

---
+++ interfaces/interfaces.d.ts
@@ -0,0 +1,24 @@
+interface Movie {
+  id: number;
+  title: string;
+  adult: boolean;
+  backdrop_path: string;
+  genre_ids: numberp[];
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  release_date: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+}
+
+interface TrendingMovie {
+  searchTerm: string;
+  movie_id: number;
+  title: string;
+  count: number;
+  poster_url: string;
+}
