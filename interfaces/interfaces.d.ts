bf9c5d47f16f69d3597d824ff52aa58f39c79004
Karyna Pomin <115844791+KarynaPomin@users.noreply.github.com>
2026-08-10 19:45:07 +0200

    Add movie details screen and display movie information

--- interfaces/interfaces.d.ts
+++ interfaces/interfaces.d.ts
@@ -27,3 +27,61 @@ interface TrendingCardProps {
   movie: TrendingMovie;
   index: number;
 }
+
+interface MovieDetails {
+  id: number;
+  title: string;
+  original_title: string;
+  original_language: string;
+  overview: string;
+  tagline: string;
+  status: string;
+  release_date: string;
+  runtime: number;
+  adult: boolean;
+
+  backdrop_path: string | null;
+  poster_path: string | null;
+
+  popularity: number;
+  vote_average: number;
+  vote_count: number;
+
+  budget: number;
+  revenue: number;
+
+  video: boolean;
+
+  genres: {
+    id: number;
+    name: string;
+  }[];
+
+  production_companies: {
+    id: number;
+    logo_path: string | null;
+    name: string;
+    origin_country: string;
+  }[];
+
+  production_countries: {
+    iso_3166_1: string;
+    name: string;
+  }[];
+
+  spoken_languages: {
+    english_name: string;
+    iso_639_1: string;
+    name: string;
+  }[];
+
+  belongs_to_collection: {
+    id: number;
+    name: string;
+    poster_path: string | null;
+    backdrop_path: string | null;
+  } | null;
+
+  homepage: string | null;
+  imdb_id: string | null;
+}
